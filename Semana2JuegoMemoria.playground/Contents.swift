c9d10c450654da01f8f21aaef506e5c17763973a
Pedro Vargas <deerme@macbook-pro-de-maria.local>
2016-08-21 22:59:32 -0300

    Actualización

--- Semana2JuegoMemoria.playground/Contents.swift
+++ Semana2JuegoMemoria.playground/Contents.swift
@@ -1,5 +1,25 @@
-//: Playground - noun: a place where people can play
-
+/**
+    Curso: https://www.coursera.org/learn/swift/
+    Semana 2 - Tarea - Juego de Memoria
+    deerme.org
+*/
 import UIKit
 
-var str = "Hello, playground"
+for i in 0...100{
+    if i % 5 == 0 {
+        print("#\(i)Bingo!!!")
+    }
+    
+    if i % 2 == 0{
+        print("#\(i)par")
+    }
+    
+    if i % 2 == 1 {
+        print("#\(i)impar")
+    }
+    
+    if i >= 30 && i <= 40 {
+        print("#\(i)Viva Swift!!!")
+    }
+    
+}
